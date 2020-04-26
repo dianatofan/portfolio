d6da960efbb19a508e0ddb52b0e3ebd4d5333378
Diana Tofan <diana_tofan@ymail.com>
2020-04-26 20:28:39 +0200

    Update page

--- src/pages/time-series.js
+++ src/pages/time-series.js
@@ -1,30 +1,24 @@
 import React from "react";
 import Header from "../components/header";
-import { graphql, navigate, useStaticQuery } from "gatsby";
-import Img from "gatsby-image";
 import "../styles/about.scss";
-import spark from "../../static/svg/spark.svg";
+import { User, Users, ChevronUp } from "react-feather";
 import timeSeriesHeader from "../images/timeSeriesHeaderCrop.png";
+import useCaseScenario from "../images/useCaseScenario.png";
+import Snakke from "react-snakke";
 
 export default ({ location }) => {
-  const data = useStaticQuery(graphql`
-  query {
-    imageOne: file(relativePath: { eq: "timeSeriesHeaderCrop.png" }) {
-      childImageSharp {
-         fluid(maxWidth: 3000, quality: 100) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-  }
-`);
-
   return (
     <div>
+      <Snakke
+        color="#5455d6"
+        top="0"
+        height="3px"
+        zIndex="10"
+      />
+      {/*<ChevronUp size={40} color="#223458" style={{ cursor: "pointer", position: "fixed", right: "40px", bottom: "40px" }}/>*/}
       <Header location={location} />
       <div className="full-screen-header">
         <img src={timeSeriesHeader} alt="" style={{ maxHeight: "60vh", width: "100%", display: "inline-block" }} />
-        {/*<Img fluid={data.imageOne.childImageSharp.fluid} style={{ maxHeight: "60vh", width: "100%", display: "inline-block" }} />*/}
       </div>
       <section className="title-section">
         <h1>Visual Exploration of Time Series in Healthcare</h1>
@@ -76,7 +70,41 @@ export default ({ location }) => {
             with minimum effort and maximum results?</div>
           <div className="quote-mark" style={{ textAlign: "right"}}>”</div>
         </blockquote>
-
+      </section>
+      <section className="problem" style={{ background: "white" }}>
+        <h1>Analysis</h1>
+        <div>
+          <div>
+            <h2>Target users</h2>
+            <div>
+              There are two groups of people who usually engage in a self-monitoring process:
+              <div className="user-types">
+                <div>
+                  <User size={100} color="#223458" />
+                  <div style={{ color: "#223458" }}><b>Private self-trackers</b> who collect information about themselves voluntarily</div>
+                </div>
+                <div>
+                  <Users size={100} color="#223458"/>
+                  <div style={{ color: "#223458" }}><b>Individuals</b> who are pushed into self-tracking by others</div>
+                </div>
+              </div>
+              This tool focuses on the second category, hence it involves many different actors (e.g. patient
+              and medical practitioner) and aims to improve the communication between them
+              through visual aids.
+            </div>
+          </div>
+          <div style={{ marginTop: "50px" }}>
+            <h2>Use case scenario</h2>
+            <div>
+              This application primarily focuses on solving problems from the healthcare industry
+              and helps medical entities diagnose and treat their patients. For instance it could be used
+              by a PTSD patient who is regularly attending therapeutic sessions. Each time he encounters outbursts,
+              he records the episodes using a one-button device and shares the collected data with his therapist
+              for a better diagnosis and treatment.
+              <img src={useCaseScenario} alt="" style={{ width: "100%" }} />
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   )
