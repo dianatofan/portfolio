24dc825456b3f67187880f9cd90278e033166a2e
Diana Tofan <diana_tofan@ymail.com>
2022-01-14 20:15:04 +0100

    Update env var name

--- src/@mkitio/gatsby-theme-password-protect/components/PasswordProtect.js
+++ src/@mkitio/gatsby-theme-password-protect/components/PasswordProtect.js
@@ -13,7 +13,7 @@ const PasswordProtect = () => {
 
   const onSubmit = (event) => {
     event.preventDefault();
-    if (password === process.env.ACCESS_KEY) {
+    if (password === process.env.GATSBY_ACCESS_KEY) {
       document.cookie = `${COOKIE_NAME}=${password}`;
       setPasswordCorrect(true);
       window.location.reload(); // eslint-disable-line
