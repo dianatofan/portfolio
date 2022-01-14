d0891ec83f91f49c67b0cf5957450cbbcae435b0
Diana Tofan <diana_tofan@ymail.com>
2022-01-14 10:20:06 +0100

    Add validation

--- src/@mkitio/gatsby-theme-password-protect/components/PasswordProtect.js
+++ src/@mkitio/gatsby-theme-password-protect/components/PasswordProtect.js
@@ -9,30 +9,49 @@ const COOKIE_NAME = "gatsby-theme-password-protect";
 
 const PasswordProtect = () => {
   const [password, setPassword] = useState("");
+  const [passwordCorrect, setPasswordCorrect] = useState(false);
 
   const onSubmit = (event) => {
     event.preventDefault();
-    document.cookie = `${COOKIE_NAME}=${password}`;
-    window.location.reload(); // eslint-disable-line
+    if (password === "notreadyyet") {
+      document.cookie = `${COOKIE_NAME}=${password}`;
+      setPasswordCorrect(true);
+      window.location.reload(); // eslint-disable-line
+    } else {
+      document.getElementById("button").classList.add("animate__shakeX");
+      setTimeout(function () {
+        document.getElementById("button").classList.remove("animate__shakeX");
+      }, 1000);
+      setPasswordCorrect(false);
+    }
   };
 
   return (
-    <div className="wrapper animate__animated animate__fadeIn">
-      <Lock width={60} height={60} />
-      <h1>This content is protected</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          name="password"
-          type="password"
-          placeholder="Enter password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          className="input"
-        />
-        <button type="submit" className="submit-button">
-          Let me in
-        </button>
-      </form>
+    <div>
+      {passwordCorrect ? null : (
+        <div className="wrapper animate__animated animate__fadeIn">
+          <Lock width={60} height={60} />
+          <h1>This content is protected</h1>
+          <form onSubmit={onSubmit}>
+            <input
+              name="password"
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              className="input"
+              id="input"
+            />
+            <button
+              type="submit"
+              className="submit-button animate__animated"
+              id="button"
+            >
+              Let me in
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
